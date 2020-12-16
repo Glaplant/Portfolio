43aad3008ea1b872e9e80094b85e0f312143fd59
glaplant <laplant.g@gmail.com>
2020-12-16 14:56:07 -0600

    updates

--- src/lib/zeldaUtillities.js
+++ src/lib/zeldaUtillities.js
@@ -3,7 +3,7 @@
 export async function getData(url,page="page=0"){
     const baseUrl = "https://zelda-api.apius.cc/api/";
     const proxy = "https://cors-anywhere.herokuapp.com/";
-    //const proxy = "http://127.0.0.1:8080/";
+   // const proxy = "http://127.0.0.1:8080/";
     const limit = "limit=20";
    // const name = "?=The Legend of Zelda";
     const newUrl =  proxy + baseUrl + url + limit + "&" + page ;
@@ -32,11 +32,11 @@ export async function getData(url,page="page=0"){
 
 
 export function hide(){
-    const element = document.querySelector(".zelda_sideNav");;
+    const element = document.querySelector("div.zelda_sideNav");;
     console.log(element);
     const menu = element;
     console.log(menu);
-    menu.classList.toggle("hide");
+    menu.classList.toggle("hideZelda");
        
 
 
